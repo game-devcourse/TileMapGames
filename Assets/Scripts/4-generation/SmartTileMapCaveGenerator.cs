a919439c907e37f5ae50bbb56589fd4c22499dc3
Hadar Bitan <hadarbitan92@gmail.com>
2024-02-14 12:41:18 +0200

    final upload

--- Assets/Scripts/4-generation/SmartTileMapCaveGenerator.cs
+++ Assets/Scripts/4-generation/SmartTileMapCaveGenerator.cs
@@ -22,15 +22,15 @@ public class SmartTileMapCaveGenerator : MonoBehaviour
     [Tooltip("For how long will we pause between each simulation step so we can look at the result?")]
     [SerializeField] float pauseTime = 1f;
 
-    //we want to have the player transform so we can randomaly place the player
     [SerializeField] Transform playerTransform = null;
     [SerializeField] int minReachableTiles = 100;
     [SerializeField] int maxIterations = 1000;
 
     [SerializeField] AllowedTiles allowedTiles = null;
 
-    private CaveGenerator caveGenerator;
     private TilemapGraph tilemapGraph;
+    private CaveGenerator caveGenerator;
+    private bool validStartingPointFound = false;
 
 
     void Start()
@@ -39,37 +39,40 @@ public class SmartTileMapCaveGenerator : MonoBehaviour
         Random.InitState(100);
         caveGenerator = new CaveGenerator(randomFillPercent, gridSize);
 
-        FindAndSetValidStartingPoint();
+        //such like in the script from the class, generate and test that it is working
+        caveGenerator.RandomizeMap();
+        GenerateAndDisplayTexture(caveGenerator.GetMap());
+
+        InvokeRepeating("FindAndSetValidStartingPointCoroutine", 0f, pauseTime);
+
     }
 
-
-    private void FindAndSetValidStartingPoint()
+    private void FindAndSetValidStartingPointCoroutine()
     {
-        bool validStartingPointFound = false;
-
-        //as long as we don't have a random point to place the player so he can reach at least minReachableTiles we continue generate the map
-        while (!validStartingPointFound)
+        if(validStartingPointFound)
         {
-            //such like in the script from the class, generate and test that it is working
-            caveGenerator.RandomizeMap();
-            GenerateAndDisplayTexture(caveGenerator.GetMap());
-
-            //getting a random position for the player to move to
-            Vector3Int playerStartingPoint = GetRandomTilePosition();
-
-            //checking if at least minReachableTiles are reachable from the random position
-            if (IsStartingPointValid(playerStartingPoint))
-            {
-                //if does place the player in the random position
-                playerTransform.position = playerStartingPoint;
-                validStartingPointFound = true;
-            }
-
-            // Schedule the next iteration of FindAndSetValidStartingPoint after the specified pauseTime so we can pause and see what's going on
-            Invoke("FindAndSetValidStartingPoint", pauseTime);
-
-            //if not continue on generate maps
+            // Stop the repeating invocation when the maximum number of iterations is reached
+            CancelInvoke("FindAndSetValidStartingPointCoroutine");
         }
+            
+        //Calculate the new values
+        caveGenerator.SmoothMap();
+
+        //Generate texture and display it on the plane
+        GenerateAndDisplayTexture(caveGenerator.GetMap());
+
+        //getting a random position for the player to move to
+        Vector3Int playerStartingPoint = GetRandomTilePosition();
+
+        //checking if at least minReachableTiles are reachable from the random position
+        if (IsStartingPointValid(playerStartingPoint))
+        {
+            //if does place the player in the random position
+            playerTransform.position = playerStartingPoint;
+            validStartingPointFound = true;
+        }
+
+        //if not continue on generate maps
     }
 
     /**
@@ -77,6 +80,34 @@ public class SmartTileMapCaveGenerator : MonoBehaviour
     *the function uses the BFS algorithm to check if the path include at least minReachableTiles
     **/
     private bool IsStartingPointValid(Vector3Int startingPoint)
+    {
+        return BFS(startingPoint);
+    }
+
+    //A function to generate a random position to the player
+    private Vector3Int GetRandomTilePosition()
+    {
+        Vector3Int randomPosition = new Vector3Int(Random.Range(0, gridSize),Random.Range(0, gridSize),0);
+        return randomPosition;
+    }
+
+    //this next function is just like in the script from the class
+    private void GenerateAndDisplayTexture(int[,] data)
+    {
+        for (int y = 0; y < gridSize; y++)
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                var position = new Vector3Int(x, y, 0);
+                var tile = data[x, y] == 1 ? wallTile : floorTile;
+                tilemap.SetTile(position, tile);
+            }
+        }
+        tilemap.RefreshAllTiles();
+    }
+
+    //the BFS algorithm with a liitle bit of change
+    public bool BFS(Vector3Int startingPoint)
     {
         Queue<Vector3Int> openQueue = new Queue<Vector3Int>();
         HashSet<Vector3Int> openSet = new HashSet<Vector3Int>();
@@ -111,27 +142,5 @@ public class SmartTileMapCaveGenerator : MonoBehaviour
             }
         }
         return false;
-    }
-
-    //A function to generate a random position to the player
-    private Vector3Int GetRandomTilePosition()
-    {
-        Vector3Int randomPosition = new Vector3Int(Random.Range(0, gridSize),Random.Range(0, gridSize),0);
-        return randomPosition;
-    }
-
-    //this next function is just like in the script from the class
-    private void GenerateAndDisplayTexture(int[,] data)
-    {
-        for (int y = 0; y < gridSize; y++)
-        {
-            for (int x = 0; x < gridSize; x++)
-            {
-                var position = new Vector3Int(x, y, 0);
-                var tile = data[x, y] == 1 ? wallTile : floorTile;
-                tilemap.SetTile(position, tile);
-            }
-        }
-        tilemap.RefreshAllTiles();
     }
 }
